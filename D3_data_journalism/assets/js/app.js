8066d1025af01bb7ad49ac8d2f46b2fedd732fab
Stephen Eldridge <eldridge.sje@gmail.com>
2021-12-14 15:32:13 -0600

    Initial commit.

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,15 @@
+// @TODO: YOUR CODE HERE!
+
+
+//Load data
+
+//Define data for scatter plot
+
+//Design scatter plot
+
+//Add scatter plot interaction
+
+//Add tool tips
+
+//Attach scatter plot to html file
+
